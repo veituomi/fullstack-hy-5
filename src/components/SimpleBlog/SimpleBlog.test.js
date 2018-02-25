ec6829b0675656e549b573cdd1d26f0b48d3321c
Veini Tuominen <veini.tuominen@hotmail.com>
2018-02-25 12:18:40 +0200

    Tehtävä 5.13

--- src/components/SimpleBlog/SimpleBlog.test.js
+++ src/components/SimpleBlog/SimpleBlog.test.js
@@ -3,19 +3,32 @@ import { shallow } from 'enzyme'
 import { SimpleBlog } from './SimpleBlog'
 
 describe.only('<SimpleBlog />', () => {
-	it('renders content', () => {
-		const blog = {
-			title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
-			author: 'Matti',
-			likes: 667,
-			url: 'https://fullstack-hy.github.io/osa5/'
-		}
+	const blog = {
+		title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+		author: 'Matti',
+		likes: 667,
+		url: 'https://fullstack-hy.github.io/osa5/'
+	}
 
+	it('renders content', () => {
 		const blogComponent = shallow(<SimpleBlog blog={blog} />)
 		const contentDiv = blogComponent.find('.content')
 
 		expect(contentDiv.text()).toContain(blog.author)
 		expect(contentDiv.text()).toContain(blog.title)
 		expect(contentDiv.text()).toContain(blog.likes)
+	})
+
+	it('can be liked', () => {
+		const mockHandler = jest.fn()
+		const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+		const button = blogComponent.find('button')
+
+		const count = 2
+		for (let i = 0; i < count; i += 1) {
+			button.simulate('click')
+		}
+
+		expect(mockHandler.mock.calls.length).toBe(count)
 	})
 })
